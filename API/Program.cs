5c00d250dfcfbb1c3ca74bce3cf6c0168177e274
Zed <kahsayzeray@gmail.com>
2021-11-07 23:01:48 +0100

    Migrations and Configuration added

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,8 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host =  CreateHostBuilder(args).Build();
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
